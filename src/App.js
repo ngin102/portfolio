f84e453121bace72dac2edd4c254c14aa6a4d982
Nicholas Gin <ngin102@uottawa.ca>
2023-05-13 23:50:31 -0400

    Pages now reset scroll position when changed, update styling

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import About from "./routes/About"
 import Projects from "./routes/Projects"
 import { Route, Routes, useLocation } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import ScrollToTop from "./components/ScrollToTop"
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <div className="content">
         <Routes>
