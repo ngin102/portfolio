dcf429db35684af7f2c97855b9bbf471f5490b7a
Nicholas Gin <ngin102@uottawa.ca>
2023-05-13 20:50:19 -0400

    Updates

---
+++ precache-manifest.6fa9962f0d1c74dd1f06407b68db012f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce4fce964c75967d09ad",
+    "url": "/static/css/main.91aef930.chunk.css"
+  },
+  {
+    "revision": "ce4fce964c75967d09ad",
+    "url": "/static/js/main.ce4fce96.chunk.js"
+  },
+  {
+    "revision": "367ecc6d4f27c05ad5bb",
+    "url": "/static/js/1.367ecc6d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f79c21101055d0abc11609ac1fc0b1d",
+    "url": "/static/media/background.1f79c211.gif"
+  },
+  {
+    "revision": "c977aaae69b07dba06c3de813a2eeeb8",
+    "url": "/static/media/portfolio_pic.c977aaae.png"
+  },
+  {
+    "revision": "af2989ccc59ebd3295250826adc9e110",
+    "url": "/static/media/service_novigrad.af2989cc.png"
+  },
+  {
+    "revision": "c3196961db590dab5fbd227c6162c950",
+    "url": "/index.html"
+  }
+];
