8c07dd89f40a84f0e49dc845e0d4916e1de97139
Nicholas Gin <ngin102@uottawa.ca>
2023-05-14 22:20:07 -0400

    Remove unnecessary import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { HashRouter } from "react-router-dom"
-import Home from './routes/Home.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
